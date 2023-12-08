8a25784a773bc82c021a4feeec4dca177e4b5393
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2023-12-08 18:01:27 +0000

    Faulty code push

--- ThreeFindParts.java
+++ ThreeFindParts.java
@@ -1,93 +1,90 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ThreeFindParts {
 
-	private int sum = 0;
+	private static int pointer = 0;
+	private static int sum = 0;
 	// Linked List to store and view part of the file
-	private LinkedList<String> buffer = new LinkedList<>();
+	private static LinkedList<String> buffer = new LinkedList<>();
 	// For Checking all 8 directions for a valid symbol
-	int[][] coordinates = new int[][] {
+	private static int[][] coordinates = new int[][]{
 		// Top : buffer.get(1).charAt(i);
-		{ 1, 0 },
+		{-1, 0},
 		// Bottom : buffer.get(3).charAt(i);
-		{ 3, 0 },
+		{1, 0},
 		// Left : buffer.get(2).charAt(i - 1);
-		{ 2, -1 },
+		{0, -1},
 		// Right : buffer.get(2).charAt(i + 1);
-		{ 2, 1 },
+		{0, 1},
 		// TopLeft : buffer.get(1).charAt(i - 1);
-		{ 1, -1 },
+		{-1, -1},
 		// TopRight : buffer.get(1).charAt(i + 1);
-		{ 1, 1 },
+		{-1, 1},
 		// BottomLeft : buffer.get(3).charAt(i - 1);
-		{ 3, -1 },
+		{1, -1},
 		// BottomRight : buffer.get(3).charAt(i + 1);
-		{ 3, 1 }
+		{1, 1}
 	};
 
 	// Searches all directions for a symbol
 	public static Boolean CheckValidParts(String number, int index) {
-		int currentLenght = number.length() + index;
-		for (int i = index; i < currentLenght; i++){
-			for ( int[] currCord: coordinates) {
-				char c = buffer.get(currCord[0]).charAt(i + currCord[1])
-				if (!Character.isDigit() && c != '.') return true;
+		int currentLength = number.length() + index - 1;
+		for (int i = index; i < currentLength; i++) {
+			for (int[] currCord : coordinates) {
+				int bufferCord = pointer + currCord[0];
+				int charNo = i + currCord[1];
+				if (charNo == 3 && bufferCord == )
+				// First Case -> two levels down -> Skip top coordinates
+				if (bufferCord < 0 || bufferCord >= buffer.size()) continue;
+				// Skipping the left most and right most checks
+				if (charNo < 0 || charNo > currentLength) continue;
+				char c = buffer.get(bufferCord).charAt(charNo);
+				if (!Character.isDigit(c) && c != '.') return true;
 			}
 		}
 		return false;
 	}
 
 	// Checks if middle line has valid parts in it
-	public static int SumOfValidPartsAtPointer (buffer) {
-		if (buffer.size() != 3) {
-			throw new IllegalStateException("Algorithm is checked at the wrong point");
-		}
-
+	public static void SumOfBuffer() {
 		Pattern pattern = Pattern.compile("\\d+");
-		Matcher matcher = pattern.matcher(buffer.get(1));
+		Matcher matcher = pattern.matcher(buffer.get(pointer));
 
-		while(matcher.find()) {			
-            String number = matcher.group(); // Get the matched number
-            int index = matcher.start();    // Get the start index of the match
+		while (matcher.find()) {
+			String number = matcher.group(); // Get the matched number
+			int index = matcher.start();    // Get the start index of the match
+			System.out.println("This is current number: " + number + " and its start index: " + index);
 			Boolean foundValid = CheckValidParts(number, index);
 
 			if (foundValid) {
+				System.out.println("This is a valid number: " + number);
 				sum += Integer.parseInt(number);
 			}
 		}
 
 	}
 
-	public static void SumOfBuffer() {
-		// Edge cases -> When file starts and ends
-		// Check second line of the linked list and run algorithm on it
-		List<Integer> validParts = new List<>();
-		// Edge cases 1 - For first line
-		if (buffer.size() == 2) {
-			CheckPartsAtTheStart();
-		} else {
-			SumOfValidPartsAtPointer();
-		}
-
-	}
-
-
 	public static void main(String[] args) {
 		try {
 			File myFile = new File("PartsInformation.txt");
 			Scanner myScan = new Scanner(myFile);
-			while (myScan.hasNextLine()){
+			while (myScan.hasNextLine()) {
 				String line = myScan.nextLine();
 				buffer.add(line);
-				if(buffer.size() > 3) {
-					buffer.remove();
-				}
+				if (buffer.size() == 3) pointer++;
+				if (buffer.size() > 3) buffer.remove();
+				SumOfBuffer();
 			}
+			pointer++;
+			SumOfBuffer();
+			System.out.println("This is the sum -> " + sum);
 			myScan.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred: "+ e);
+			System.out.println("An error occurred: " + e);
 		}
 	}
 }
